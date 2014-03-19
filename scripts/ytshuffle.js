dfe7f15ca5d852df22c3ad154cddcc128159be30
Sam Agnew <sagnew@eden.rutgers.edu>
2014-03-19 17:26:12 -0400

    Shuffle script mostly working

--- scripts/ytshuffle.js
+++ scripts/ytshuffle.js
@@ -1,4 +1,5 @@
 var tracks = [];
+var tracksInOrder = [];
 var current = 0;
 
 //The Knuth shuffling algorithm
@@ -27,16 +28,24 @@ var shuffle = function(array) {
 //Function that populates the tracks array with jQuery <a> objects.
 var getTrackTimes = function(){
     tracks = [];
-    $('#eow-description').find('a').each(function(){
+    $('#eow-description').find('a').each(function(index){
         if($(this).text().indexOf(":") !== -1){
-            tracks.push($(this));
+            //track is the jQUery object, index is the track number
+            tracks.push({
+                "track": $(this),
+                "index": index
+            });
+            tracksInOrder.push({
+                "track": $(this),
+                "index": index
+            });
         }
     });
     shuffle(tracks);
 }();
 
 var playNextTrack = function(){
-    tracks[current].click();
+    tracks[current].track.click();
     current += 1;
     if(current >= tracks.length){
         //Re-shuffle the array
@@ -45,21 +54,35 @@ var playNextTrack = function(){
     }
 };
 
-//Play on repeat indefinitely for now with no way of stopping
-var listenToCurrentTrack = function(){
-    var timeArray = tracks[current].text().split(":");
+//Figure out how long the current track plays for
+var getPlayTime = function(){
+    currentTrack = tracks[current];
+    var timeArray = currentTrack.track.text().split(":");
     var minutes = timeArray[0];
     var seconds = timeArray[1];
-    var playTime = (minutes*60 + seconds)*1000;
+    var currentTrackTime = (minutes*60 + seconds);
+
+    //Now get the time of the previous track
+    if(currentTrack.index !== 0){
+        timeArray = tracksInOrder[currentTrack.index - 1].track.text().split(":");
+    }else{
+        timeArray = tracksInOrder[tracksInOrder.length - 1].track.text().split(":");
+    }
+
+    minutes = timeArray[0];
+    seconds = timeArray[1];
+    var previousTrackTime = (minutes*60 + seconds);
+
+    //The difference between the two is the playtime
+    return (currentTrackTime - previousTrackTime)*1000;
+};
+
+//Play on repeat indefinitely for now with no way of stopping
+var listenToCurrentTrack = function(){
 
     playNextTrack();
 
     setTimeout(function(){
-        timeArray = tracks[current].text().split(":");
-        minutes = timeArray[0];
-        seconds = timeArray[1];
-        playTime = (minutes*60 + seconds)*1000;
-
-        setTimeout(listenToCurrentTrack, playTime);
-    }, playTime);
-}();
+        listenToCurrentTrack();
+    }, getPlayTime());
+};
