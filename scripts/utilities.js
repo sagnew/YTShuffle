bde6fcc3b95c62eb5743a16819171d36d5647e66
Sam Agnew <sagnew@eden.rutgers.edu>
2014-03-21 19:10:16 -0400

    Add setTimeout replacement and move unrelated functions to utilities.js

---
+++ scripts/utilities.js
@@ -0,0 +1,40 @@
+//A replacement for setTimeout that is able to be paused.
+function Timer(callback, delay) {
+    var timerId, start, remaining = delay;
+
+    this.pause = function() {
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    this.resume = function() {
+        start = new Date();
+        timerId = window.setTimeout(callback, remaining);
+    };
+
+    this.resume();
+}
+
+//The Knuth shuffling algorithm
+var shuffle = function(array) {
+
+    var currentIndex = array.length
+        , temporaryValue
+        , randomIndex
+        ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
--- scripts/ytshuffle.js
+++ scripts/ytshuffle.js
@@ -2,30 +2,6 @@ var tracks = [];
 var tracksInOrder = [];
 var current = 0;
 
-//The Knuth shuffling algorithm
-var shuffle = function(array) {
-
-    var currentIndex = array.length
-        , temporaryValue
-        , randomIndex
-        ;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
 //Function that populates the tracks array with jQuery <a> objects.
 var getTrackTimes = function(){
     tracks = [];
